cd20a1eef6c062df0ffed4962f65be4e805a4c3f
dalia-alawneh <alawnehdalia340@gmail.com>
2023-07-06 12:24:46 +0200

    Display products from api

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const categoriesList = document.getElementById('categories');
+const productsSection = document.getElementById('products');
 
 async function getAllCategories() {
     try {
@@ -10,7 +11,41 @@ async function getAllCategories() {
     }
 }
 
+async function getAllProducts() {
+    try {
+        const response = await fetch('https://dummyjson.com/products');
+        const products = await response.json();
+        displayProducts(products.products);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getAllProducts();
 getAllCategories();
+
+function displayProducts(products) {
+    let result = '';
+    for (const product of products) {
+        result += `
+        <div class="col-md-4">
+            <div class="card p-2">
+                <a href="#"><img src="${product.images[0]}" class="card-img-top img-fluid img-responsive" alt="Image 1"></a>
+                <div class="card-body">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div>
+                            <h5 class="card-title">${(product.title).split(' ').slice(0,2).join(' ')}</h5>
+                            <p class="card-text">${product.category}</p>
+                        </div>
+                        <span>$ ${product.price}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    productsSection.innerHTML = result;
+}
 
 function displayCategories(categories) {
     let result = `<a href="#" class="list-group-item list-group-item-action active">All</a>`;
