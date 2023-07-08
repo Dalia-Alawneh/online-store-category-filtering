94d952a434d630f02caa52a83f9d8a5649669e3c
dalia-alawneh <alawnehdalia340@gmail.com>
2023-07-09 00:22:59 +0200

    Refactor index js code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,36 @@
 const categoriesList = document.getElementById('categories');
 const productsSection = document.getElementById('products');
-
+const searchProductForm = document.getElementById('search-product-form')
 async function getAllCategories() {
     try {
         const response = await fetch('https://dummyjson.com/products/categories');
-        const categories = await response.json();
-        displayCategories(categories);
+        return await response.json();
     } catch (error) {
         console.error(error);
     }
 }
+
 
 async function getAllProducts() {
     try {
         const response = await fetch('https://dummyjson.com/products');
         const products = await response.json();
-        displayProducts(products.products);
+        return products.products;
     } catch (error) {
         console.error(error);
     }
 }
 
-getAllProducts();
-getAllCategories();
+async function searchProduct(query) {
+    try {
+        const response = await fetch(`https://dummyjson.com/products/search?q=${query}`);
+        const filteredProducts = await response.json();
+        return filteredProducts.products;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 
 function displayProducts(products) {
     let result = '';
@@ -34,7 +42,7 @@ function displayProducts(products) {
                 <div class="card-body">
                     <div class="d-flex align-items-center justify-content-between">
                         <div>
-                            <h5 class="card-title">${(product.title).split(' ').slice(0,2).join(' ')}</h5>
+                            <h5 class="card-title">${(product.title).split(' ').slice(0, 2).join(' ')}</h5>
                             <p class="card-text">${product.category}</p>
                         </div>
                         <span>$ ${product.price}</span>
@@ -48,9 +56,26 @@ function displayProducts(products) {
 }
 
 function displayCategories(categories) {
+    console.log(categories);
     let result = `<a href="#" class="list-group-item list-group-item-action active">All</a>`;
     for (const category of categories) {
         result += `<a href="#" class="list-group-item list-group-item-action">${category}</a>`;
     }
     categoriesList.innerHTML = result;
 }
+
+
+searchProductForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const query = searchProductForm.elements.search.value;
+    const products = await searchProduct(query);
+    displayProducts(products);
+});
+
+
+async function generateData(callback, display) {
+    let data = await callback()
+    display(data)
+}
+generateData(getAllCategories, displayCategories)
+generateData(getAllProducts, displayProducts)
