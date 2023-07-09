327284228d5ea094307302952a089d7d9f97ecd2
dalia-alawneh <alawnehdalia340@gmail.com>
2023-07-09 02:57:25 +0200

    Add filter by category functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,3 @@
-const categoriesList = document.getElementById('categories');
-const productsSection = document.getElementById('products');
 const searchProductForm = document.getElementById('search-product-form')
 async function getAllCategories() {
     try {
@@ -9,8 +7,6 @@ async function getAllCategories() {
         console.error(error);
     }
 }
-
-
 async function getAllProducts() {
     try {
         const response = await fetch('https://dummyjson.com/products');
@@ -20,7 +16,15 @@ async function getAllProducts() {
         console.error(error);
     }
 }
-
+async function getProductsByCategory(category) {    
+    try {
+        const response = await fetch(`https://dummyjson.com/products/category/${category}`);
+        const products =  await response.json();
+        return products.products
+    } catch (error) {
+        console.error(error);
+    }
+}
 async function searchProduct(query) {
     try {
         const response = await fetch(`https://dummyjson.com/products/search?q=${query}`);
@@ -33,41 +37,51 @@ async function searchProduct(query) {
 
 
 function displayProducts(products) {
+    const productsSection = document.getElementById('products');
     let result = '';
-    for (const product of products) {
-        result += `
-        <div class="col-md-4">
-            <div class="card p-2">
-                <a href="#"><img src="${product.images[0]}" class="card-img-top img-fluid img-responsive" alt="Image 1"></a>
-                <div class="card-body">
-                    <div class="d-flex align-items-center justify-content-between">
-                        <div>
-                            <h5 class="card-title">${(product.title).split(' ').slice(0, 2).join(' ')}</h5>
-                            <p class="card-text">${product.category}</p>
+    if(products.length > 0){
+        for (const product of products) {
+            result += `
+            <div class="col-md-4">
+                <div class="card p-2">
+                    <a href="#"><img src="${product.images[0]}" class="card-img-top img-fluid img-responsive" alt="Image 1"></a>
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div>
+                                <h5 class="card-title">${(product.title).split(' ').slice(0, 2).join(' ')}</h5>
+                                <p class="card-text">${product.category}</p>
+                            </div>
+                            <span>$ ${product.price}</span>
                         </div>
-                        <span>$ ${product.price}</span>
                     </div>
                 </div>
             </div>
-        </div>
-        `;
+            `;
+        }
+    }else{
+        result = '<div class="d-flex justify-content-center"><p>No data found :)</p></div>'
     }
+   
     productsSection.innerHTML = result;
 }
 
-function displayCategories(categories) {
+function renderCategoriesWithListeners(categories) {
+    const categoriesList = document.getElementById('categories');
     console.log(categories);
-    let result = `<a href="#" class="list-group-item list-group-item-action active">All</a>`;
+    let result = `<button class="list-group-item list-group-item-action active">All</button>`;
     for (const category of categories) {
-        result += `<a href="#" class="list-group-item list-group-item-action">${category}</a>`;
+        result += `<button class="list-group-item list-group-item-action">${category}</button>`;
     }
     categoriesList.innerHTML = result;
+    let  listGroupItems = document.querySelectorAll('.list-group button');
+    addClickListenerToBtn(listGroupItems);
 }
 
 
 searchProductForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const query = searchProductForm.elements.search.value;
+    const query = searchProductForm.elements[0].value;
+    console.log(query);
     const products = await searchProduct(query);
     displayProducts(products);
 });
@@ -77,5 +91,16 @@ async function generateData(callback, display) {
     let data = await callback()
     display(data)
 }
-generateData(getAllCategories, displayCategories)
+generateData(getAllCategories, renderCategoriesWithListeners)
 generateData(getAllProducts, displayProducts)
+
+function addClickListenerToBtn(listGroupItems){
+    listGroupItems.forEach((listGroupItem) => {
+        listGroupItem.addEventListener('click', async () => {
+            const category = listGroupItem.textContent.trim()
+            const products = await getProductsByCategory(category);
+            displayProducts(products)
+        })
+    })
+}
+
